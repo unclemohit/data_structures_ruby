dd9f9913ce9f3360be1dff932f0558f89440804e
unclemohit <mohit26998@gmail.com>
2025-06-10 15:03:18 -0700

    Added custom stack class

---
+++ stack.rb
@@ -0,0 +1,31 @@
+class Stack
+  def initialize
+    @store = []  # We'll manage this array manually
+  end
+end
+
+def push(item)
+  @store[@store.length] = item
+
+end
+def pop()
+  #if null condition
+  return nil if @store.empty?
+  
+  #so since you need to print you store the last element
+  last = @store[@store.length-1] 
+
+  #now we remove
+  #
+  @store=@store[0..-1]
+  last
+
+end
+
+def empty?
+   @store.length == 0
+end
+
+def peek
+   @store[@store.length - 1]
+end
